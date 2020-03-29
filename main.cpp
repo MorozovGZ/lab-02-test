68cbb1d299e5a07cf8674785bd0ed6047ebf13ab
Bob (MorozovGZ) <bob@example.com>
2020-03-29 21:02:59 +0300

    UPD: min bob

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@ using namespace std;
 int main()
 {
    cout << "Enter A and B: ";
-int a, b,max;
+
+int a, b,max,min;
+
+
+
 
 cin >> a >> b;
 cout<<"a+b="<<a+b<<endl;
 cout<<"a-b="<<a-b<<endl;
 cout<<"a*b="<<a*b<<endl;
 cout<<"a/b="<<a/b<<endl;
+
 max=b;
 {
 
@@ -20,5 +25,14 @@ if(a>b)
  max=a;
 }
 cout<<"max="<<max;
+
+min=b;
+{
+
+if(a<b)
+ min=a;
+}
+cout<<"min="<<min;
+
     return 0;
 }
