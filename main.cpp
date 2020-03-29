1a9b0763f752f11b2a67c13bf213d04df941cf22
Alice (MorozovGZ) <alice@example.com>
2020-03-29 20:51:05 +0300

    UPd MAX Alice

--- main.cpp
+++ main.cpp
@@ -6,12 +6,19 @@ using namespace std;
 int main()
 {
    cout << "Enter A and B: ";
-int a, b;
+int a, b,max;
 
 cin >> a >> b;
 cout<<"a+b="<<a+b<<endl;
 cout<<"a-b="<<a-b<<endl;
 cout<<"a*b="<<a*b<<endl;
-cout<<"a/b="<<a/b;
+cout<<"a/b="<<a/b<<endl;
+max=b;
+{
+
+if(a>b)
+ max=a;
+}
+cout<<"max="<<max;
     return 0;
 }
